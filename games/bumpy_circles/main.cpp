4cd9d81e3b7d11c0910f8cfb178934e3eddd01a6
Logan Troyer <troyerlogan@gmail.com>
2019-08-10 00:30:27 -0400

    Maded it so circle is blue and in the center of the screen

--- games/bumpy_circles/main.cpp
+++ games/bumpy_circles/main.cpp
@@ -5,11 +5,29 @@ int main() {
     sf::VideoMode desktopMode = sf::VideoMode::getDesktopMode();
     const float SCREEN_W = desktopMode.width;
     const float SCREEN_H = desktopMode.height;
+
+    const int USER_R = 50;
     sf::RenderWindow window(sf::VideoMode(SCREEN_W, SCREEN_H), "Bumpy Circles", sf::Style::Fullscreen);
+
+    /*
+     CIRCLE SHAPE
+     */
+    sf::CircleShape circle(USER_R);
+    
+    // radius and point count can be set after as well
+    //shape.setRadius(100);
+    //shape.setPointCount(3);
+    
+    // Red, Green and Blue from 0 to 255
+    circle.setFillColor(sf::Color(66,135,245));
+    circle.setOrigin(50,50);
+
+    circle.setPosition(SCREEN_W/2, SCREEN_H/2);
 
     while (window.isOpen()) {
 
         sf::Event event;
+
         while (window.pollEvent(event)) {
 
           switch (event.type) {
@@ -35,6 +53,8 @@ int main() {
         }
 
         window.clear();
+
+        window.draw(circle);
 
         window.display();
 
