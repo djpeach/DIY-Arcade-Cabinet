cdccfe1fd995bb07461c53b541316ead76659a36
Daniel Peach <dpeaches96@gmail.com>
2019-09-04 14:14:46 -0400

    showing values by re-loading font

--- games/cube_checkers/Cube.cpp
+++ games/cube_checkers/Cube.cpp
@@ -6,21 +6,16 @@
 int counter = 0;
 
 Cube::Cube(sf::Vector2f size, sf::Vector2f pos, sf::Color color) :
-value(1), font(), rect(), valueText("", font, 40) {
+value(1), font(), rect(), valueText("", font, 60) {
   rect.setSize(size);
   rect.setOrigin(size.x / 2, size.y / 2);
   rect.setPosition(pos);
   rect.setFillColor(color);
-
-  if (!font.loadFromFile("assets/font/Regular.ttf")) {
-    std::cerr << "Cube could not load font from file: assets/font/Regular.ttf" << std::endl;
-    exit(1);
-  }
-  // std::string valueString = std::to_string(value);
-  valueText.setString("1");
-  // sf::FloatRect valueTextBounds = valueText.getGlobalBounds();
-  // valueText.setOrigin(valueTextBounds.width / 2, valueTextBounds.height / 2);
-  valueText.setPosition(rect.getPosition());
+  std::string valueString = std::to_string(value);
+  valueText.setString(valueString);
+  sf::FloatRect valueTextBounds = valueText.getGlobalBounds();
+  valueText.setOrigin(valueTextBounds.width / 2, valueTextBounds.height / 2);
+  valueText.setPosition(rect.getPosition().x - 12, rect.getPosition().y - 30);
 }
 
 sf::Vector2f Cube::getSize() {
@@ -29,5 +24,12 @@ sf::Vector2f Cube::getSize() {
 
 void Cube::draw(sf::RenderWindow & window) {
   window.draw(rect);
-  // window.draw(valueText);
+
+  if (!font.loadFromFile("assets/font/Regular.ttf")) {
+    std::cerr << "Cube could not load font from file: assets/font/Regular.ttf" << std::endl;
+    exit(1);
+  }
+  valueText.setFont(font);
+
+  window.draw(valueText);
 }
