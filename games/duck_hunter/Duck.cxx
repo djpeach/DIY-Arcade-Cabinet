f795f2850f4ee64560b641ee8448da187535d05b
Daniel Peach <dpeaches96@gmail.com>
2019-09-05 15:59:05 -0400

    added timer and score

--- games/duck_hunter/Duck.cxx
+++ games/duck_hunter/Duck.cxx
@@ -22,6 +22,9 @@ namespace djpeach {
     this->setPosition((rand() % (int)(SCREEN_WIDTH / 2)) + (SCREEN_WIDTH / 4), SCREEN_HEIGHT - GROUND_HEIGHT);
 
     this->xSpeed = this->getPosition().x < SCREEN_WIDTH / 2 ? DUCK_SPEED : -DUCK_SPEED;
+    if(this->xSpeed < 0) {
+      this->scale(-1.f, 1.f);
+    }
     this->ySpeed = -1 * DUCK_SPEED;
     this->flying = false;
   }
@@ -30,7 +33,11 @@ namespace djpeach {
     sf::VideoMode desktopMode = sf::VideoMode::getDesktopMode();
     const float SCREEN_WIDTH = desktopMode.width;
     const float SCREEN_HEIGHT = desktopMode.height;
+    this->setScale(3, 3);
     this->xSpeed = this->getPosition().x < SCREEN_WIDTH / 2 ? DUCK_SPEED : -DUCK_SPEED;
+    if(this->xSpeed < 0) {
+      this->scale(-1.f, 1.f);
+    }
     this->ySpeed = -1 * DUCK_SPEED;
   }
 
